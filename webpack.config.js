4415a32c677cc3cebb2818cfc34e37b2a1a85db8
Anthony Pilloud <pilloud.anthony@gmail.com>
2019-01-05 21:44:41 +0100

    [func] add start files

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const devMode = process.env.NODE_ENV !== 'production';
+
+module.exports = {
+  entry: {
+    style: './src/scss/style.scss'
+  },
+  module: {
+    rules: [{
+      test: /\.(sa|sc|c)ss$/,
+      use: [
+        MiniCssExtractPlugin.loader,
+        'css-loader',
+        'sass-loader',
+      ],
+    }],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: devMode ? 'css/[name].css' : 'css/[name].[hash].css',
+      chunkFilename: devMode ? 'css/[id].css' : 'css/[id].[hash].css',
+    })
+  ],
+  optimization: {
+    minimizer: [
+      new OptimizeCSSAssetsPlugin({})
+    ]
+  },
+};
